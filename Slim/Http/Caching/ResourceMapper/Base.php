6cf2bc7a75f669e82ac5ca7769090e5ef5f74717
maegnes <MagnusBuk@gmx.de>
2013-08-19 23:48:06 +0200

    changed setHeaders to abstract method
    set setHeaders in ResourceMapper to abstract and implemented it in base
    class

--- Slim/Http/Caching/ResourceMapper/Base.php
+++ Slim/Http/Caching/ResourceMapper/Base.php
@@ -137,23 +137,7 @@ abstract class Base implements IResourceMapper {
      * @access public
      * @return void
      */
-    public function setHeaders() {
-
-        $this->_prepareResource();
-
-        $res = $this->getHandler()->read( $this->_resource );
-
-        if( $res instanceof \Slim\Http\Caching\IResource ) {
-
-            // Set ETag
-            $this->getApplication()->etag( $res->getEtag() );
-
-            // Also set the "expires"-Header
-            $this->getApplication()->expires( '+' . $res->getLifetime() . ' hours' );
-
-        }
-
-    }
+    abstract public function setHeaders();
 
     /**
      * (1) Prepares the resource uri and formats it to correct syntax.
--- Slim/Http/Caching/ResourceMapper/Etag.php
+++ Slim/Http/Caching/ResourceMapper/Etag.php
@@ -33,4 +33,22 @@ namespace Slim\Http\Caching\ResourceMapper;
 
 class Etag extends Base {
 
+    public abstract function setHeaders() {
+
+        $this->_prepareResource();
+
+        $res = $this->getHandler()->read( $this->_resource );
+
+        if( $res instanceof \Slim\Http\Caching\IResource ) {
+
+            // Set ETag
+            $this->getApplication()->etag( $res->getEtag() );
+
+            // Also set the "expires"-Header
+            $this->getApplication()->expires( '+' . $res->getLifetime() . ' hours' );
+
+        }
+
+    }
+
 }
